4043f8e2771807417dd0ba48214e2b41d3b7af3d
Hao <haodt1990@gmail.com>
2016-11-13 00:50:10 +0700

    initial deploy

---
+++ web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	log.Println("Index request", r.RemoteAddr, r.URL)
+	t, _ := template.ParseFiles("views/index.html")
+	t.Execute(w, nil)
+}
+
+func main() {
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", index).Methods("GET")
+
+	http.Handle("/", r)
+
+	log.Println("Listening at 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
