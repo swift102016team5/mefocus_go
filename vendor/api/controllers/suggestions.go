9a57b52bb4dc251098a392def34ef3fb86e873f7
Hao <haodt1990@gmail.com>
2016-11-18 00:53:12 +0700

    add suggestions api

--- vendor/api/controllers/suggestions.go
+++ vendor/api/controllers/suggestions.go
@@ -1,13 +1,40 @@
 package controllers
 
 import (
-	"log"
+	"encoding/json"
 	"net/http"
-	"html/template"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
+type Suggestion struct {
+	ID        		bson.ObjectId 	`json:"id" bson:"_id"`
+	Name      		string 			`json:"name"`
+	Duration     	string 			`json:"duration"`
+}
+
+type SuggestionsResponse struct {
+	Data []Suggestion `json:"data"`
+}
+
 func Suggestions(w http.ResponseWriter, r *http.Request) {
-	log.Println("Index request", r.RemoteAddr, r.URL)
-	t, _ := template.ParseFiles("views/index.html")
-	t.Execute(w, nil)
+	session, err := mgo.Dial("mongodb://mefocus:mefocus@ds157247.mlab.com:57247/mefocus")
+    if err != nil {
+        panic(err)
+    }
+
+    defer session.Close()
+    
+    session.SetMode(mgo.Monotonic, true)
+
+    collection := session.DB("mefocus").C("suggestions")
+	result := []Suggestion{}
+
+	err = collection.Find(nil).All(&result)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type","application/vnd.api+json")
+	json.NewEncoder(w).Encode(SuggestionsResponse{result})
 }
